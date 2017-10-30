b49b3269b4d968e7fbd35e6834ed9d88ddaef074
DJ <DJ>
2017-10-30 19:15:59 +0100

    The main class we use to launch the application.

--- src/fuckGitHub.java
+++ src/fuckGitHub.java
@@ -1,6 +1,8 @@
 public class fuckGitHub {
-    public static void fuck(){
-        for(int i=0;i<1000;i++)
-        System.out.println("Fuck gitHub");
+
+    public static void main(String[] args) {
+            for(int i=0;i<1000;i++)
+                System.out.println("Fuck gitHub");
+        }
     }
-}
+
