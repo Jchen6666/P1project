b749097228a0f3fd734fe6dedfbac5c1a5417c23
Kangur <pbpiotrek@vp.pl>
2017-11-23 10:57:18 +0100

    Added BossFight class to work on.

---
+++ src/BossFight.java
@@ -0,0 +1,27 @@
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class BossFight extends BasicGameState {
+
+
+        public BossFight(int state) {
+
+        }
+
+        public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+
+        }
+
+        public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+
+        }
+
+        public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+
+        }
+
+        public int getID() { return 0; }
+
+}
+
